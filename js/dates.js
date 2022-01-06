ada00235463ed52059f47de1e43fef8bb2d5051a
ShishigamiZero <shishigamizero@hotmail.com>
2022-01-05 21:42:06 -0700

    Moved Stylesheet, Added content to index, added dates.js file

---
+++ js/dates.js
@@ -0,0 +1,5 @@
+const lastmod = document.querySelector('#lastmod');
+lastmod.innerHTML = `Last Modified: ${document.lastModified}`;
+
+const year = document.querySelector('#year');
+year.textContent = document.year; 
