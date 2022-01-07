dc2a25d8f3a3d0272446cf43c8450cd820fc206c
ShishigamiZero <shishigamizero@hotmail.com>
2022-01-06 18:18:03 -0700

    changed js year function to use querySelector.

--- js/dates.js
+++ js/dates.js
@@ -2,5 +2,5 @@ const lastmod = document.querySelector('#lastmod');
 lastmod.innerHTML = `Last Modified: ${document.lastModified}`;
 
 //This will get the year.
-const year = document.querySelector("year")
+const year = document.querySelector("#year")
 year.innerHTML = new Date().getFullYear();
