9ab3c35da04cb8d953a451b8ed2915b796f27898
ShishigamiZero <shishigamizero@hotmail.com>
2022-01-06 18:12:41 -0700

    testing

--- js/dates.js
+++ js/dates.js
@@ -2,4 +2,4 @@ const lastmod = document.querySelector('#lastmod');
 lastmod.innerHTML = `Last Modified: ${document.lastModified}`;
 
 //This will get the year.
-document.getElementById("year").innerHTML = new Date().getFullYear();
+document.querySelector("year").innerHTML = new Date().getFullYear();
