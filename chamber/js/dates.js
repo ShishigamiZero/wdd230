9f91024ba741690a8b2aafda6a0de232da2688e9
ShishigamiZero <shishigamizero@hotmail.com>
2022-01-27 22:11:56 -0700

    getting to work on my chamber home page.

---
+++ chamber/js/dates.js
@@ -0,0 +1,15 @@
+const lastmod = document.querySelector('#lastmod');
+lastmod.innerHTML = `Last Modification: ${document.lastModified}`;
+
+//This will get the year.
+const year = document.querySelector("#year")
+year.innerHTML = new Date().getFullYear();
+
+const datefield = document.querySelector("date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<strong>${fulldate}</strong>`;
