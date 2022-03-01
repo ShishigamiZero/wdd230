dbe8ab41ed47690b3ca441c1f0371b59816ee1ed
ShishigamiZero <shishigamizero@hotmail.com>
2022-02-28 17:26:07 -0700

    testing js

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,5 +1,5 @@
 const requestURL = "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
-const cards = document.querySelector('.cards');
+//const cards = document.querySelector('.cards');
 
 fetch(requestURL)
   .then(function (response) {
