8a7116e52f99795fd5ffa4e5cbdb37460db517f0
ShishigamiZero <shishigamizero@hotmail.com>
2022-03-01 18:56:34 -0700

    added if statement to prophets.js to fix alt

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -31,7 +31,30 @@ fetch(requestURL)
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portait of  ${prophet.name} ${prophet.lastname} - Latter-day Saint President #${prophet.order}`);
+    if (prophet.order == 1) {
+      portrait.setAttribute(
+        "alt",
+        `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}st Latter-day President`
+      );
+    }
+    else if (prophet.order == 2) {
+      portrait.setAttribute(
+          "alt",
+          `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}nd Latter-day President`
+        );
+    }
+    else if (prophet.order == 3) {
+      portrait.setAttribute(
+          "alt",
+          `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}rd Latter-day President`
+        );
+    }
+    else {
+      portrait.setAttribute(
+          "alt",
+          `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`
+        );
+    }
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
