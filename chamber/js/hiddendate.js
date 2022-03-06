a366aec8b4842b8f61f44be4eeaf14efefef8311
ShishigamiZero <shishigamizero@hotmail.com>
2022-03-05 21:08:28 -0700

    fixed date submission on join page

--- chamber/js/hiddendate.js
+++ chamber/js/hiddendate.js
@@ -1,2 +1,4 @@
-// Use to add a date to the Join Form Page. 
-document.getElementById("current_date").innerHTML = Date();
+// Use to add a date to the Join Form Page.
+const current_date = new Date();
+let date = document.getElementById("current_date").innerHTML = Date();
+document.querySelector('#current_date').value = date;
