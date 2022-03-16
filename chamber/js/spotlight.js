3278136f3c15bafd734d47fefa080aa7e685ee46
ShishigamiZero <shishigamizero@hotmail.com>
2022-03-15 18:09:20 -0600

    fixed up js with tutor help. just need to fix css

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -7,13 +7,102 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-      
+    let goldview;
+    let silverview;
+    let bronzeview;
+    let dayOfWeek = new Date().getDay();
+    let directoryArray = [];
+
     const directories = jsonObject["directory"];
-    directories.forEach(displayDirectories);
+    // directories.forEach(displayDirectories);    
+
+  if (dayOfWeek == 0) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold1'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver1'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze1'
+    );
+  } else if (dayOfWeek == 1) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold1'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver1'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze2'
+    );
+  } else if (dayOfWeek == 2) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold2'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver2'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze3'
+    );
+  } else if (dayOfWeek == 3) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold2'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver2'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze4'
+    );
+  } else if (dayOfWeek == 4) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold3'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver3'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze5'
+    );
+  } else if (dayOfWeek == 5) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold3'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver3'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze1'
+    );
+  } else if (dayOfWeek == 6) {
+    goldview = directories.find(
+      (directory) => directory.memberlevel == 'gold4'
+    );    
+    silverview = directories.find(
+      (directory) => directory.memberlevel == 'silver4'
+    );
+    bronzeview = directories.find(
+      (directory) => directory.memberlevel == 'bronze2'
+    );
+  } else {
+    newsheader = 'We apologize, there was an unknown error.'
+    }
+  
+  directoryArray.push(goldview);
+  directoryArray.push(silverview);
+  directoryArray.push(bronzeview); 
+
+  console.log(directoryArray);
+  directoryArray.forEach(displayDirectories);
+
   });
 
   function displayDirectories(directory) {
     // Create elements to add to the document
+    let section = document.createElement('section');
+    let spots = document.createElement('section');
     let spot = document.createElement("section");
     let h2 = document.createElement("h2");
     let p1 = document.createElement("p1");
@@ -27,58 +116,24 @@ fetch(requestURL)
     p2.innerHTML = `${directory.phone} <br>`;
     p3.innerHTML = `<strong>Founder:</strong> ${directory.founder} <br>`
 
-  
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', directory.imageurl);
     portrait.setAttribute("alt",`Logo for ${directory.name}`);
     portrait.setAttribute('loading', 'lazy');
-  
+
+    // Add classes to HTML elements created
+    section.classList.add('contentbox');
+    spots.classList.add('spots');
+    
     // Add/append the section(card) with the h2 element
     spot.appendChild(h2);
     spot.appendChild(portrait);
     spot.appendChild(p1);
     spot.appendChild(p2);
     spot.appendChild(p3);
+    spots.appendChild(spot);
+    section.appendChild(spots);
     
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector("div.spots").appendChild(spot);
- 
- let dayOfWeek;
-dayOfWeek = now.getDay();
-if (dayOfWeek == 0) {
-  goldview = directory.memberlevel("gold1");
-  silverview = directory.memberlevel("silver1");
-  bronzeview = directory.memberlevel("bronze1");
-} else if (dayOfWeek == 1) {
-    goldview = directory.memberlevel("gold1");
-    silverview = directory.memberlevel("silver1");
-    bronzeview = directory.memberlevel("bronze2");
-} else if (dayOfWeek == 2) {
-    goldview = directory.memberlevel("gold2");
-    silverview = directory.memberlevel("silver2");
-    bronzeview = directory.memberlevel("bronze3");
-} else if (dayOfWeek == 3) {
-    goldview = directory.memberlevel("gold2");
-    silverview = directory.memberlevel("silver2");
-    bronzeview = directory.memberlevel("bronze4");
-} else if (dayOfWeek == 4) {
-    goldview = directory.memberlevel("gold3");
-    silverview = directory.memberlevel("silver3");
-    bronzeview = directory.memberlevel("bronze5");
-} else if (dayOfWeek == 5) {
-    goldview = directory.memberlevel("gold3");
-    silverview = directory.memberlevel("silver3");
-    bronzeview = directory.memberlevel("bronze1");
-} else if (dayOfWeek == 6) {
-    goldview = directory.memberlevel("gold4");
-    silverview = directory.memberlevel("silver4");
-    bronzeview = directory.memberlevel("bronze2");
-} else {
-  newsheader = 'We apologize, there was an unknown error.'
+    document.querySelector(".spotdiv").appendChild(section);
   }
-
-document.querySelector(".goldview").textContent = goldview;
-document.querySelector(".silverview").textContent = silverview;
-document.querySelector(".bronzeview").textContent = bronzeview;    
-  }
-
