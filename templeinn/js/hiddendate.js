51de21524be9934c053458d9fe0ac54119bf1f02
ShishigamiZero <shishigamizero@hotmail.com>
2022-04-02 12:00:30 -0600

    contact page done, moving to reservations

---
+++ templeinn/js/hiddendate.js
@@ -0,0 +1,4 @@
+// Use to add a date to the Join Form Page.
+const current_date = new Date();
+let date = document.getElementById("current_date").innerHTML = Date();
+document.querySelector('#current_date').value = date;
