03574ee77ff258a9e1fc3a3dfe895f3659ff553f
ShishigamiZero <shishigamizero@hotmail.com>
2022-04-04 17:27:47 -0600

    finished temples.js for temple cards

--- templeinn/js/temples.js
+++ templeinn/js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = "https://shishigamizero.github.io/wdd230/chamber/data/data.json";
+const requestURL = "https://shishigamizero.github.io/wdd230/templeinn/data/temples.json";
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
@@ -8,11 +8,11 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
       
-    const directories = jsonObject["directory"];
-    directories.forEach(displayDirectories);
+    const temples = jsonObject["temple"];
+    temples.forEach(displayTemples);
   });
 
-  function displayDirectories(directory) {
+  function displayTemples(temple) {
     // Create elements to add to the document
     let card = document.createElement("section");
     let h2 = document.createElement("h2");
@@ -22,20 +22,28 @@ fetch(requestURL)
     let p4 = document.createElement("p4");
     let p5 = document.createElement("p5");
     let p6 = document.createElement("p6");
+    let p7 = document.createElement("p7");
+    let p8 = document.createElement("p8");
+    let p9 = document.createElement("p9");
+    let p10 = document.createElement("p10");
     let portrait = document.createElement("img");
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = directory.name;
-    p1.innerHTML = `${directory.streetaddress} ${directory.city}, ${directory.state} ${directory.zipcode}<br>`;
-    p2.innerHTML = `${directory.phone} <br>`;
-    p3.innerHTML = `${directory.website} <br>`;
-    p4.innerHTML = `<strong>Type: </strong>${directory.businesstype} <br>`;
-    p5.innerHTML = `<strong>Opened:</strong> ${directory.yearopened} <br>`;
-    p6.innerHTML = `<strong>Founder:</strong> ${directory.founder} <br>`;
-  
+    h2.textContent = temple.name;
+    p1.innerHTML = `${temple.streetaddress} ${temple.city}, ${temple.state} ${temple.zipcode}, ${temple.country}<br>`;
+    p2.innerHTML = `${temple.phone} <br>`;
+    p3.innerHTML = `${temple.website} <br>`;
+    p4.innerHTML = `<strong>Ordinance Schedule:</strong> ${temple.ordschedule} <br>`;
+    p5.innerHTML = `<strong>Session Schedule:</strong> ${temple.sesschedule} <br>`;
+    p6.innerHTML = `<strong>President:</strong> ${temple.president} <br>`;
+    p7.innerHTML = `<strong>Announcement:</strong> ${temple.announcement}`;
+    p8.innerHTML = `<strong>Groundbreaking:</strong> ${temple.groundbreaking}`;
+    p9.innerHTML = `<strong>Dedication:</strong> ${temple.dedication}`;
+    p10.innerHTML = `<strong>Temple Closure Dates:</strong> ${temple.closchedule}`;
+
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', directory.imageurl);
-    portrait.setAttribute("alt",`Logo for ${directory.name}`);
+    portrait.setAttribute('src', temple.imageurl);
+    portrait.setAttribute("alt",`The ${temple.name}`);
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
@@ -47,7 +55,10 @@ fetch(requestURL)
     card.appendChild(p4);
     card.appendChild(p5);
     card.appendChild(p6);
-
+    card.appendChild(p7);
+    card.appendChild(p8);
+    card.appendChild(p9);
+    card.appendChild(p10);
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector("div.cards").appendChild(card);
