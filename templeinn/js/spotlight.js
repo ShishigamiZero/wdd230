613653cdf58b6e590bf9b6a064db99e463df44a0
ShishigamiZero <shishigamizero@hotmail.com>
2022-04-05 18:32:48 -0600

    adding content to home page

--- templeinn/js/spotlight.js
+++ templeinn/js/spotlight.js
@@ -64,14 +64,18 @@ fetch(requestURL)
     let p2 = document.createElement("p2");
     let p3 = document.createElement("p3");
     let p4 = document.createElement("p4");
+    let p5 = document.createElement("p5");
+    let p6 = document.createElement("p6");
     let portrait = document.createElement("img");
   
     // Change the textContent property of the h2 element to contain the business's full name
     h2.textContent = temple.name;
-    p1.innerHTML = `${temple.streetaddress} <br>`;
-    p2.innerHTML = `${temple.phone} <br>`;
-    p3.innerHTML = `<strong>President:</strong> ${temple.president} <br>`
-    p4.innerHTML = `<strong>Dedication:<strong> ${temple.dedication} `
+    p1.innerHTML = `<br>${temple.streetaddress} <br>`;
+    p2.innerHTML = `${temple.city}, ${temple.state} ${temple.zipcode} <br>`;
+    p3.innerHTML = `${temple.country} <br>`;
+    p4.innerHTML = `${temple.phone} <br>`;
+    p5.innerHTML = `<strong>President:</strong> ${temple.president} <br>`
+    p6.innerHTML = `<strong>Dedication:<strong> ${temple.dedication} `
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', temple.imageurl);
@@ -85,10 +89,12 @@ fetch(requestURL)
     // Add/append the section(card) with the h2 element
     spot.appendChild(h2);
     spot.appendChild(portrait);
-    spot.appendChild(p4);
     spot.appendChild(p1);
     spot.appendChild(p2);
     spot.appendChild(p3);
+    spot.appendChild(p4);
+    spot.appendChild(p5);
+    spot.appendChild(p6);
     spots.appendChild(spot);
     section.appendChild(spots);
     
